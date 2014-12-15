43b17bfb7d811a4185e1c6d0865d93b914a4eca6
Bhushan Waghode <bhushan.waghode@synerzip.com>
2014-12-15 14:15:18 +0530

    Third party integrated successfully

--- CheckPointStudy/RootViewController.swift
+++ CheckPointStudy/RootViewController.swift
@@ -12,7 +12,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        println("My first line in swift");
         // Do any additional setup after loading the view, typically from a nib.
     }
 
