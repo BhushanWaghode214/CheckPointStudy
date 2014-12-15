144c887d585169b471d83af934971c942e12d3be
Bhushan Waghode <bhushan.waghode@synerzip.com>
2014-12-15 13:17:37 +0530

    Initial Comment for starting project

--- CheckPointStudy/ViewController.swift
+++ CheckPointStudy/ViewController.swift
@@ -12,11 +12,13 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        println("My first line in swift");
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        
         // Dispose of any resources that can be recreated.
     }
 
